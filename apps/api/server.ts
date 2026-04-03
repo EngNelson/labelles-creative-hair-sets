5823dda4c5efce2fe5cde1a428d20f681cf4c902
EngNelson <bechem200@gmail.com>
2026-04-03 12:34:12 +0100

    fix

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -223,6 +223,6 @@ app.use(errorHandler);
 
 // Start server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen( PORT as any,  '0.0.0.0',() => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
